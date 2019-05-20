8a993c3d84bdd06677c63d91a019a9381dd5ed30
fogi89188 <viktorpetrov02@abv.bg>
2019-05-20 19:34:09 +0300

    adding more logic to the main function and making an end loop

--- DnDTools/Program.cs
+++ DnDTools/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DnDTools
@@ -9,10 +10,44 @@ namespace DnDTools
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             CharacterStatGeneratorClass cs = new CharacterStatGeneratorClass();
-            cs.GenerateAllNTimes(15);
-            Console.WriteLine(cs.ToString());
+            string command = "";
+            while (command != "end")
+            {
+                string[] commandArr = Console.ReadLine().Split().ToArray();
+                switch (commandArr[0])
+                {
+                    case "stat":
+                        {
+                            try
+                            {
+                                cs.GenerateAllNTimes(int.Parse(commandArr[1]));
+                                Console.WriteLine(cs.ToString());
+                            }
+                            catch(Exception)
+                            {
+                                cs.GenerateAllNTimes(1);
+                                Console.WriteLine(cs.ToString());
+                            }
+                            break;
+                        }
+                    case "initiative":
+                        {
+                            break;
+                        }
+                    case "end":
+                        {
+                            return;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid Command...");
+                            Thread.Sleep(500);
+                            break;
+                        }
+                }
+            }
         }
     }
 }
